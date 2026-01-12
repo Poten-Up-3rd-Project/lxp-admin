3d34077dd506f2076ae3b12a62ad050b9d649eae
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-12 16:58:14 +0900

    first commit

---
+++ src/test/java/com/lxp/admin/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxp.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
