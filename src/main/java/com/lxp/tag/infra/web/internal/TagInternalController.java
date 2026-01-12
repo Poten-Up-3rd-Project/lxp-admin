dfc05805ecabf67bbbb639de0b6f390cb85e8e5c
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-12 19:27:01 +0900

    feat(tag): 태그 조회 internal api 구현

---
+++ src/main/java/com/lxp/tag/infra/web/internal/TagInternalController.java
@@ -0,0 +1,83 @@
+package com.lxp.tag.infra.web.internal;
+
+import com.lxp.tag.application.service.TagQueryService;
+import com.lxp.tag.application.port.out.dto.TagResult;
+import com.lxp.tag.infra.web.external.request.FindTagsByIdsRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/internal/api-v1/tags")
+public class TagInternalController {
+
+    private final TagQueryService tagQueryService;
+
+    public TagInternalController(TagQueryService tagQueryService) {
+        this.tagQueryService = tagQueryService;
+    }
+
+    @GetMapping
+    ResponseEntity<List<TagResult>> findAll() {
+        return ResponseEntity.ok(tagQueryService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<TagResult> findById(
+            @PathVariable
+            @Valid
+            @NotNull(message = "id 는 null 일 수 없습니다.")
+            Long id
+    ) {
+        Optional<TagResult> optional = tagQueryService.findById(id);
+        if (optional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(optional.get());
+    }
+
+    @GetMapping("/findByIds")
+    ResponseEntity<List<TagResult>> findByIds(
+            @RequestBody
+            @Valid
+            FindTagsByIdsRequest request
+    ) {
+        return ResponseEntity.ok(tagQueryService.findByIds(request.ids()));
+    }
+
+    @GetMapping("/findByName")
+    ResponseEntity<Long> findByName(
+            @RequestParam
+            @Valid
+            @NotNull(message = "id 는 null 일 수 없습니다.")
+            String name
+    ) {
+        Optional<Long> optional = tagQueryService.findByName(name);
+        if (optional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Long body = optional.get();
+        return ResponseEntity.ok(body);
+    }
+
+    @GetMapping("/search")
+    ResponseEntity<List<TagResult>> search(
+            @RequestParam
+            @Valid
+            @NotBlank
+            String q
+    ) {
+        return ResponseEntity.ok(tagQueryService.searchIdsByNameContaining(q));
+    }
+}
